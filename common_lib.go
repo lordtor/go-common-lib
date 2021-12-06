836d601bcd16e0a5eb851009c7ed8cafcba22f9f
Yuriy Rumyantsev <rumyanec@gmail.com>
2021-12-06 12:02:27 +0300

    add ne methods, test, examples

--- common_lib.go
+++ common_lib.go
@@ -1,11 +1,20 @@
+/*
+ Base common func`s
+*/
+
 package go_common_lib
 
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"sort"
+	"strings"
 )
 
+// A function to check slice contain item string
+// The return value is bool
 func SliceContain(slice []string, item string) bool {
 	exist := false
 	for sliceID := range slice {
@@ -16,6 +25,9 @@ func SliceContain(slice []string, item string) bool {
 	}
 	return exist
 }
+
+// A function for update exist slice not contain item string
+// The return value is new slice
 func UpdateList(slice []string, item string) []string {
 	if !SliceContain(slice, item) {
 		slice = append(slice, item)
@@ -23,10 +35,12 @@ func UpdateList(slice []string, item string) []string {
 	return slice
 }
 
+// A function convert slice to sting
+// The return value is string
 func StringFromList(list []string) (st string) {
 	for i := 0; i < len(list); i++ {
 		if st == "" {
-			st = fmt.Sprintf("%s", list[i])
+			st = list[i]
 		} else {
 			st = fmt.Sprintf("%s,%s", st, list[i])
 		}
@@ -34,6 +48,9 @@ func StringFromList(list []string) (st string) {
 	}
 	return st
 }
+
+// A function for update exist slice values from anothe slice
+// The return value is updated slice
 func UpdateStructList(main []string, in []string) []string {
 	for inID := range in {
 		id := inID
@@ -42,8 +59,131 @@ func UpdateStructList(main []string, in []string) []string {
 	return main
 }
 
+// A function for calculate string hash sum by sha256
+// The return value is hash string
 func CalcHash(aStr string) string {
 	hash := sha256.New()
 	hash.Write([]byte(aStr))
 	return hex.EncodeToString(hash.Sum(nil))
+}
+
+// A function for sort slice map`s
+// The return value is sorted slice
+func SortedMap(m []map[string]string) (out []map[string]string) {
+	var keys []string
+	for i := range m {
+		for k := range m[i] {
+			keys = append(keys, k)
+		}
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		for i := range m {
+			for kk, v := range m[i] {
+				if k == kk {
+					out = append(out, map[string]string{k: v})
+					break
+				}
+			}
+		}
+	}
+	return out
+}
+
+// A function for convert interface to slice strings
+// The return value is slice strings
+func ListConvert(in interface{}) ([]string, error) {
+	var out []string
+	r, err := json.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(r, &out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// A function for update slince map by not exist map
+// The return value is updated slice
+func UpdateMapList(sliceMaps []map[string]string, itemMap map[string]string) []map[string]string {
+	update := false
+	if len(sliceMaps) == 0 {
+		sliceMaps = append(sliceMaps, itemMap)
+	} else {
+		for sliceID := range sliceMaps {
+			for k, v := range sliceMaps[sliceID] {
+				for kk, vv := range itemMap {
+					if k != kk {
+						sliceMaps = append(sliceMaps, itemMap)
+						update = true
+						break
+					} else if k == kk && v != vv {
+						sliceMaps[sliceID][k] = vv
+						update = true
+						break
+					}
+				}
+				if update {
+					break
+				}
+			}
+			if update {
+				break
+			}
+		}
+	}
+	return sliceMaps
+}
+
+// A function for trim first & last space
+// The return value is trimed string
+func StandardizeSpaces(s string) string {
+	return strings.Join(strings.Fields(s), " ")
+}
+func UpdateListBySplit(slice []string, item string, spliters []string) []string {
+	split := false
+	char := ""
+	if len(spliters) == 0 {
+		spliters = []string{",", "\\", "/"}
+	}
+
+	for i := range spliters {
+		if strings.Contains(item, spliters[i]) {
+			split = true
+			char = spliters[i]
+		}
+	}
+	if split {
+		words := strings.Split(item, char)
+		if len(words) > 0 {
+			for id := range words {
+				w := StandardizeSpaces(words[id])
+				if !SliceContain(slice, w) {
+					slice = append(slice, w)
+				}
+			}
+		}
+	} else {
+		if !SliceContain(slice, item) {
+			w := StandardizeSpaces(item)
+			slice = append(slice, w)
+		}
+
+	}
+	return slice
+}
+
+func MapListConvert(in interface{}) ([]map[string]string, error) {
+	var out []map[string]string
+	r, err := json.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(r, &out)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
--- common_lib_test.go
+++ common_lib_test.go
@@ -1,6 +1,7 @@
 package go_common_lib
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -36,8 +37,8 @@ func TestSliceContain(t *testing.T) {
 		args args
 		want bool
 	}{
-		{"True", args{testSlice, "123"}, true},
-		{"False", args{testSlice, "23"}, true},
+		{"Contain", args{testSlice, "123"}, true},
+		{"Not contain", args{testSlice, "23"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -60,8 +61,8 @@ func TestUpdateList(t *testing.T) {
 		args args
 		want []string
 	}{
-		{"Not exist", args{testSlice, "23"}, testSlice2},
-		{"Not Exist", args{testSlice, "123"}, testSlice},
+		{"Updated list", args{testSlice, "23"}, testSlice2},
+		{"Not updated list", args{testSlice, "123"}, testSlice},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -73,8 +74,9 @@ func TestUpdateList(t *testing.T) {
 }
 
 func TestStringFromList(t *testing.T) {
-	testSlice := []string{"123", "23"}
+	testSlice := []string{"123"}
 	testSlice2 := []string{}
+	testSlice3 := []string{"123", "23"}
 	type args struct {
 		list []string
 	}
@@ -83,8 +85,9 @@ func TestStringFromList(t *testing.T) {
 		args   args
 		wantSt string
 	}{
-		{"Not exist", args{testSlice}, "123, 23"},
-		{"Not Exist", args{testSlice2}, ""},
+		{"Not empty list", args{testSlice}, "123"},
+		{"Empty list", args{testSlice2}, ""},
+		{"Not empty list2", args{testSlice3}, "123,23"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -119,4 +122,233 @@ func TestUpdateStructList(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestSortedMap(t *testing.T) {
+	type args struct {
+		m []map[string]string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantOut []map[string]string
+	}{
+		{"Un Sort", args{[]map[string]string{{"b": "b"}, {"a": "a"}}}, []map[string]string{{"a": "a"}, {"b": "b"}}},
+		{"Sort", args{[]map[string]string{{"a": "a"}, {"b": "b"}}}, []map[string]string{{"a": "a"}, {"b": "b"}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotOut := SortedMap(tt.args.m); !reflect.DeepEqual(gotOut, tt.wantOut) {
+				t.Errorf("SortedMap() = %v, want %v", gotOut, tt.wantOut)
+			}
+		})
+	}
+}
+
+func TestListConvert(t *testing.T) {
+	var names, names2, names3 interface{}
+	names = []string{"first", "second"}
+	names2 = []map[string]string{{"first": "second"}}
+	names3 = make(chan int)
+	type args struct {
+		in interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{"Not err", args{names}, []string{"first", "second"}, false},
+		{"Err", args{names2}, nil, true},
+		{"Err2", args{names3}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ListConvert(tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ListConvert() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ListConvert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUpdateMapList(t *testing.T) {
+	type args struct {
+		sliceMaps []map[string]string
+		itemMap   map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []map[string]string
+	}{
+		{"Update", args{[]map[string]string{{"a": "b"}}, map[string]string{"b": "b"}}, []map[string]string{{"a": "b"}, {"b": "b"}}},
+		{"Update 2", args{[]map[string]string{}, map[string]string{"b": "b"}}, []map[string]string{{"b": "b"}}},
+		{"Update 3", args{[]map[string]string{{"a": "b"}, {"b": "b"}}, map[string]string{"a": "c"}}, []map[string]string{{"a": "c"}, {"b": "b"}}},
+		{"Not Update", args{[]map[string]string{{"a": "b"}}, map[string]string{"a": "b"}}, []map[string]string{{"a": "b"}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UpdateMapList(tt.args.sliceMaps, tt.args.itemMap); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("UpdateMapList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStandardizeSpaces(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Trim first", args{" A A"}, "A A"},
+		{"Trim last", args{"A A "}, "A A"},
+		{"Trim all", args{" A A "}, "A A"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := StandardizeSpaces(tt.args.s); got != tt.want {
+				t.Errorf("StandardizeSpaces() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUpdateListBySplit(t *testing.T) {
+	type args struct {
+		slice    []string
+		item     string
+		spliters []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"Test1", args{[]string{}, "A,A", nil}, []string{"A"}},
+		{"Test2", args{[]string{}, "A,B", nil}, []string{"A", "B"}},
+		{"Test3", args{[]string{"A", "B"}, "A,B", nil}, []string{"A", "B"}},
+		{"Test4", args{[]string{"A", "B"}, "B\\C", nil}, []string{"A", "B", "C"}},
+		{"Test5", args{[]string{"A", "B"}, "B*C", []string{"*"}}, []string{"A", "B", "C"}},
+		{"Test5", args{[]string{"A", "B"}, "BC", []string{"*"}}, []string{"A", "B", "BC"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UpdateListBySplit(tt.args.slice, tt.args.item, tt.args.spliters); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("UpdateListBySplit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMapListConvert(t *testing.T) {
+	var names, names2, names3 interface{}
+	names = []string{"first", "second"}
+	names2 = []map[string]string{{"first": "second"}}
+	names3 = make(chan int)
+	type args struct {
+		in interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []map[string]string
+		wantErr bool
+	}{
+		{"Not err", args{names2}, []map[string]string{{"first": "second"}}, false},
+		{"Err1", args{names}, nil, true},
+		{"Err2", args{names3}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := MapListConvert(tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MapListConvert() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MapListConvert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func ExampleSliceContain() {
+	result := SliceContain([]string{"a", "b", "c"}, "b")
+	fmt.Println("Contain:", result)
+	// Output:
+	// Contain: true
+}
+func ExampleUpdateList() {
+	result := UpdateList([]string{"a", "b", "c"}, "d")
+	fmt.Println("New slice:", result)
+	// Output:
+	// New slice: [a b c d]
+}
+func ExampleStringFromList() {
+	result := StringFromList([]string{"a", "b", "c"})
+	fmt.Println("String:", result)
+	// Output:
+	// String: a,b,c
+}
+func ExampleUpdateStructList() {
+	result := UpdateStructList([]string{"a", "b"}, []string{"a", "c"})
+	fmt.Println("Updated slice:", result)
+	// Output:
+	// Updated slice: [a b c]
+}
+func ExampleCalcHash() {
+	result := CalcHash("testString")
+	fmt.Println("Hash:", result)
+	// Output:
+	// Hash: 4acf0b39d9c4766709a3689f553ac01ab550545ffa4544dfc0b2cea82fba02a3
+}
+func ExampleSortedMap() {
+	result := SortedMap([]map[string]string{{"b": "c", "c": "c", "a": "c"}})
+	fmt.Println("Sorted map:", result)
+	// Output:
+	// Sorted map: [map[a:c] map[b:c] map[c:c]]
+}
+func ExampleListConvert() {
+	var in interface{}
+	in = []string{"first", "second"}
+	result, _ := ListConvert(in)
+	fmt.Println("Slice:", result)
+	// Output:
+	// Slice: [first second]
+}
+func ExampleUpdateMapList() {
+	result := UpdateMapList([]map[string]string{{"a": "b"}}, map[string]string{"b": "b"})
+	fmt.Println("Slice:", result)
+	// Output:
+	// Slice: [map[a:b] map[b:b]]
+}
+func ExampleStandardizeSpaces() {
+	result := StandardizeSpaces(" ggg ggg ggg ")
+	fmt.Println("String:", result)
+	// Output:
+	// String: ggg ggg ggg
+}
+func ExampleUpdateListBySplit() {
+	result := UpdateListBySplit([]string{"A", "B"}, "B*C", []string{"*"})
+	fmt.Println("String:", result)
+	// Output:
+	// String: [A B C]
+}
+func ExampleMapListConvert() {
+	var names interface{}
+	names = []map[string]string{{"first": "second"}}
+	result, _ := MapListConvert(names)
+	fmt.Println("String:", result)
+	// Output:
+	// String: [map[first:second]]
 }
